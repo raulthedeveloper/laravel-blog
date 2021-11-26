c94483301947b8e7dc96c6857772bfc1cc40e848
Raul Rodriguez <raul0882@gmail.com>
2021-11-26 17:19:23 -0500

    created single post and featured post template

--- resources/views/blog/feature.blade.php
+++ resources/views/blog/feature.blade.php
@@ -2,5 +2,198 @@
 
 @section('content')
 
-<h1>I am the feature page</h1>
+<div class="container">
+    <div class="row">
+        <div class="col-md-8" style="border-right:1px solid rgb(218 218 218);">
+            <img class="w-100" style="max-height:500px" src="https://mlbostoncommon.com/get/files/image/galleries/interior-deign-spacejoy-unsplash.jpg" alt="">
+            <div class="text-center mt-3">
+                <h5>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam, ullam.</h5>
+           
+            <h2> <a class="text-dark" href="">Lorem ipsum dolor sit amet.</a></h2>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolores et explicabo quidem, distinctio consequuntur, quia eius tenetur dolorem sunt cum necessitatibus aliquam aut aperiam molestias?</p>
+            </div>
+            
+        </div>
+        <div class="col-md-4">
+            <h3>Recent Post</h3>
+            <hr>
+            <div class="row">
+                <div class="col-8"><span>Article Title</span>
+                    <h4>Lorem ipsum dolor sit amet consectetur adipisicing?</h4>
+                    <small>Jane Doe</small>
+                </div>
+                
+                <div class="col-4">
+                    <img class="img-fluid" src="https://mlbostoncommon.com/get/files/image/galleries/interior-deign-spacejoy-unsplash.jpg" alt="">
+                </div>
+                
+            </div>
+
+            <hr>
+
+            <div class="row">
+                <div class="col-8"><span>Article Title</span>
+                    <h4>Lorem ipsum dolor sit amet consectetur adipisicing?</h4>
+                    <small>Jane Doe</small>
+                </div>
+                
+                <div class="col-4">
+                    <img class="img-fluid" src="https://mlbostoncommon.com/get/files/image/galleries/interior-deign-spacejoy-unsplash.jpg" alt="">
+                </div>
+                
+            </div>
+
+            <hr>
+
+            <div class="row">
+                <div class="col-8"><span>Article Title</span>
+                    <h4>Lorem ipsum dolor sit amet consectetur adipisicing?</h4>
+                    <small>Jane Doe</small>
+                </div>
+                
+                <div class="col-4">
+                    <img class="img-fluid" src="https://mlbostoncommon.com/get/files/image/galleries/interior-deign-spacejoy-unsplash.jpg" alt="">
+                </div>
+                
+            </div>
+
+            
+        </div>
+
+        
+       
+        
+    </div>
+    <hr>
+    
+    <div class="my-5 ">
+       
+        <div class="d-flex justify-content-between">
+            <h2>Design</h2>
+        <a href="#">More Content -></a>
+        </div>
+        <div class="row">
+            <div class="col-md-4">
+                <div class="card bg-dark text-white">
+                    <img src="https://mlbostoncommon.com/get/files/image/galleries/interior-deign-spacejoy-unsplash.jpg" class="card-img" alt="...">
+                    <div class="card-img-overlay">
+                      <h5 class="card-title">Card title</h5>
+                      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                      
+                    </div>
+                  </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card bg-dark text-white">
+                    <img src="https://mlbostoncommon.com/get/files/image/galleries/interior-deign-spacejoy-unsplash.jpg" class="card-img" alt="...">
+                    <div class="card-img-overlay">
+                      <h5 class="card-title">Card title</h5>
+                      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                      
+                    </div>
+                  </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card bg-dark text-white">
+                    <img src="https://mlbostoncommon.com/get/files/image/galleries/interior-deign-spacejoy-unsplash.jpg" class="card-img" alt="...">
+                    <div class="card-img-overlay">
+                      <h5 class="card-title">Card title</h5>
+                      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                      
+                    </div>
+                  </div>
+            </div>
+        </div>
+
+        <hr>
+    </div>
+
+    <div class="my-5 ">
+        
+        <div class="d-flex justify-content-between">
+            <h2>Modern</h2>
+        <a href="#">More Content -></a>
+        </div>
+        <div class="row">
+            <div class="col-md-4">
+                <div class="card bg-dark text-white">
+                    <img src="https://mlbostoncommon.com/get/files/image/galleries/interior-deign-spacejoy-unsplash.jpg" class="card-img" alt="...">
+                    <div class="card-img-overlay">
+                      <h5 class="card-title">Card title</h5>
+                      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                      
+                    </div>
+                  </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card bg-dark text-white">
+                    <img src="https://mlbostoncommon.com/get/files/image/galleries/interior-deign-spacejoy-unsplash.jpg" class="card-img" alt="...">
+                    <div class="card-img-overlay">
+                      <h5 class="card-title">Card title</h5>
+                      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                      
+                    </div>
+                  </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card bg-dark text-white">
+                    <img src="https://mlbostoncommon.com/get/files/image/galleries/interior-deign-spacejoy-unsplash.jpg" class="card-img" alt="...">
+                    <div class="card-img-overlay">
+                      <h5 class="card-title">Card title</h5>
+                      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                      
+                    </div>
+                  </div>
+            </div>
+        </div>
+
+        <hr>
+    </div>
+
+    <div class="my-5 ">
+        <div class="d-flex justify-content-between">
+            <h2>Seasons</h2>
+        <a href="#">More Content -></a>
+        </div>
+        
+        <div class="row">
+            
+            <div class="col-md-4">
+                <div class="card bg-dark text-white">
+                    <img src="https://mlbostoncommon.com/get/files/image/galleries/interior-deign-spacejoy-unsplash.jpg" class="card-img" alt="...">
+                    <div class="card-img-overlay">
+                      <h5 class="card-title">Card title</h5>
+                      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                      
+                    </div>
+                  </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card bg-dark text-white">
+                    <img src="https://mlbostoncommon.com/get/files/image/galleries/interior-deign-spacejoy-unsplash.jpg" class="card-img" alt="...">
+                    <div class="card-img-overlay">
+                      <h5 class="card-title">Card title</h5>
+                      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                      
+                    </div>
+                  </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card bg-dark text-white">
+                    <img src="https://mlbostoncommon.com/get/files/image/galleries/interior-deign-spacejoy-unsplash.jpg" class="card-img" alt="...">
+                    <div class="card-img-overlay">
+                      <h5 class="card-title">Card title</h5>
+                      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                      
+                    </div>
+                  </div>
+            </div>
+        </div>
+
+        <hr>
+    </div>
+   
+
+    
+</div>
 @endsection
--- resources/views/blog/single.blade.php
+++ resources/views/blog/single.blade.php
@@ -3,9 +3,113 @@
 @section('content')
 
 
-
 <div class="container">
+
     <div class="row">
+        <div class="col-md-8">
+            <div class="d-flex">
+               
+
+                  <button type="button" class="btn btn-primary my-1 mr-1"><i class="fab fa-facebook"></i>Facebook</button>
+                <button type="button" class="btn btn-success m-1"><i class="fab fa-instagram"></i></i>Instagram</button>
+                <button type="button" class="btn btn-info m-1"><i class="fab fa-linkedin"></i>Linkdin</button>
+                <button type="button" class="btn btn-dark m-1"><i class="far fa-envelope"></i>Email</button>
+
+
+            </div>
+            {{-- <img class="w-100" style="max-height:500px" src="{{asset("storage/images/post_images/".$post->featured_image)}}" alt=""> --}}
+            <img class="w-100" style="max-height:500px" src="https://mlbostoncommon.com/get/files/image/galleries/interior-deign-spacejoy-unsplash.jpg" alt="">
+
+            <span>Author: Jane Doe | </span>
+            <span>Date: 3/12/22 | </span>
+            <span>Category: Design </span>
+
+            <p class="mt-3">{{$post->main_text}}</p> 
+
+
+            <h3>I am a Header</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi eveniet incidunt nihil ratione quo consectetur commodi velit tempore deleniti rem illo ipsum pariatur, consequatur nobis veniam, accusantium atque officia alias!</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi eveniet incidunt nihil ratione quo consectetur commodi velit tempore deleniti rem illo ipsum pariatur, consequatur nobis veniam, accusantium atque officia alias!</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi eveniet incidunt nihil ratione quo consectetur commodi velit tempore deleniti rem illo ipsum pariatur, consequatur nobis veniam, accusantium atque officia alias!</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi eveniet incidunt nihil ratione quo consectetur commodi velit tempore deleniti rem illo ipsum pariatur, consequatur nobis veniam, accusantium atque officia alias!</p>
+        </div>
+        <div class="col-md-4" style="background-color:#e9ecef;">
+            
+            <form class="d-flex my-5">
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success" type="submit">Search</button>
+              </form>
+
+              <div>
+                  <h5 class="font-weight-bold">Recent Blog Posts</h5>
+
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea deleniti illum reprehenderit atque praesentium</p>
+
+                  <hr>
+
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea deleniti illum reprehenderit atque praesentium</p>
+
+                  <hr>
+
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea deleniti illum reprehenderit atque praesentium</p>
+
+                  <hr>
+
+                  <h5 class="font-weight-bold mb-2">Blog Categories</h5>
+
+                  <div style="height: 300px; overflow:scroll">
+
+                    <hr>
+                    <a href="#">Lifestyle</a>
+                    
+  
+                    <hr>
+
+                    <a href="">Education</a>
+                    
+  
+                    <hr>
+                    <a href="">Festive</a>
+                   
+  
+                    <hr>
+                    <a href="">Design</a>
+                    
+
+                    <hr>
+                   
+                   
+                    <a href="#">Lifestyle</a>
+                    
+  
+                    <hr>
+
+                    <a href="">Education</a>
+                    
+  
+                    <hr>
+                    <a href="">Festive<</a>
+                   
+  
+                    <hr>
+                    <a href="">Design</a>
+                    
+
+                    <hr>
+                    <a href="">Lifestyle</a>
+                   
+  
+                    <hr>
+                  </div>
+                 
+                  
+              </div>
+
+        </div>
+    </div>
+
+
+    {{-- <div class="row">
         <img class="w-100" style="max-height:500px" src="{{asset("storage/images/post_images/".$post->featured_image)}}" alt="">
     </div>
 
@@ -14,15 +118,8 @@
     </div>
 
     <div class="row d-flex">
-<span class="badge badge-primary m-3" >Primary</span>
-<span class="badge badge-secondary m-3">Secondary</span>
-<span class="badge badge-success m-3">Success</span>
-<span class="badge badge-danger m-3">Danger</span>
-<span class="badge badge-warning m-3">Warning</span>
-<span class="badge badge-info m-3">Info</span>
-<span class="badge badge-light m-3">Light</span>
-<span class="badge badge-dark m-3">Dark</span>
-    </div>
+
+    </div> --}}
 </div>
 
 @endsection
